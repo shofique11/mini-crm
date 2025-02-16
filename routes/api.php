3e8923823a88abd4182cfa31004933a75548c785
shofique11 <programmingcode11@gmail.com>
2025-02-17 01:32:32 +0600

    counselor lead

--- routes/api.php
+++ routes/api.php
@@ -34,6 +34,7 @@ Route::middleware('auth:api')->group(function () {
         Route::get('/counselor-dashboard', function () {
             return response()->json(['message' => 'Welcome, Counselor!']);
         });
+        Route::get('counselor-leads', [LeadController::class, 'counselorLeads']);  
         Route::post('applications', [ApplicationController::class, 'store']);  // Move application (counselor only)
         Route::put('applications/{application}', [ApplicationController::class, 'update']);  // Update an application (counselor only)
     });
